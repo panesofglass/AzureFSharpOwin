4d60f92707879120a3d008de4cc5a2fb1f7971f7
Ryan Riley <ryan.riley@panesofglass.org>
2013-06-17 22:08:06 -0500

    Create a simple F# Web API hosted in an Azure Worker Role using OWIN

---
+++ WebApiRole/Startup.fs
@@ -0,0 +1,22 @@
+﻿namespace WebApiRole
+
+open Owin
+open System.Net
+open System.Net.Http
+open System.Web.Http
+
+type RouteOptions = { id: RouteParameter }
+
+type Startup() =
+    member x.Configuration(app) =
+        let config = new HttpConfiguration()
+        config.EnableSystemDiagnosticsTracing() |> ignore
+        config.Routes.MapHttpRoute("Default", "{controller}/{id}", { id = RouteParameter.Optional }) |> ignore
+        WebApiAppBuilderExtensions.UseWebApi(app, config) |> ignore
+
+type TestController() =
+    inherit ApiController()
+    member x.Get() =
+        "Hello from OWIN!"
+    member x.Get(id) =
+        sprintf "Hello from OWIN, id %d" id
