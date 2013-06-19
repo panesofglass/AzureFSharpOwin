244d9ca6be256913b9c06a5c6963d1dba2188bb0
Ryan Riley <ryan.riley@panesofglass.org>
2013-06-19 12:51:21 -0500

    Add default async handler.

--- WebApiRole/Startup.fs
+++ WebApiRole/Startup.fs
@@ -9,9 +9,18 @@ type RouteOptions = { id: RouteParameter }
 
 type Startup() =
     member x.Configuration(app: IAppBuilder) =
+        // Host Web API
         let config = new HttpConfiguration()
         config.Routes.MapHttpRoute("Default", "{controller}/{id}", { id = RouteParameter.Optional }) |> ignore
         app.UseWebApi(config) |> ignore
+
+        // Host a default handler
+        app.UseHandlerAsync(StartupExtensions.OwinHandlerAsync(fun req res ->
+            res.
+                SetHeader("ContentType", "text/plain").
+                WriteAsync("Hello, world!")
+        )) |> ignore
+
 
 type TestController() =
     inherit ApiController()
